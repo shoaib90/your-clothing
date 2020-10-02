ee7dde79d535350d893b3a8215de746cdc4026a9
Quest <shoaibkhan3412@gmail.com>
2020-10-02 21:58:31 +0530

    broken our homepage into smaller components, finished styling our menu-item component

---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./menu-item.styles.scss";
+
+const MenuItem = ({ title, imageUrl, size }) => (
+  <div className={`${size} menu-item`}>
+    <div
+      className="background-image"
+      style={{
+        backgroundImage: `url(${imageUrl})`,
+      }}
+    />
+    <div className="content">
+      <h1 className="title">{title.toUpperCase()}</h1>
+      <span className="subtitle">SHOP NOW</span>
+    </div>
+  </div>
+);
+
+// As we can see above that, we have this style attribute with our html and can pass in props value, so as to
+// making the styles dynamic using string templatig language.
+
+export default MenuItem;
